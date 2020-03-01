39bd9c4313507956142b3ca28a31b561fab8b0fc
Gucode <guc0d3@gmail.com>
2020-03-01 16:59:17 +0700

    move business to logic

--- event/contact.js
+++ event/contact.js
@@ -1,34 +1,50 @@
 const env = require('../env')
+const logic = require('../logic')
 const tool = require('../tool')
 
 const get = async (replyToken, message) => {
   if (!message) return false
   if (message.type !== 'text') return false
   if (message.text !== env.messageEvent.contact) return false
-  // console.log('[-] botEvent.contect.get')
-  let rows = await tool
-    .db('setting')
-    .where('option', 'CONTACT_IMAGE')
-    .orWhere('option', 'CONTACT_MESSAGE')
-    .orderBy('option')
-  if (rows.length != 2) return false
-  let messages = rows.reduce((total, row) => {
-    if (row.option === 'CONTACT_IMAGE' && row.value) {
-      total.push({
-        type: 'image',
-        originalContentUrl: row.value,
-        previewImageUrl: row.value
-      })
-    }
-    if (row.option === 'CONTACT_MESSAGE' && row.value) {
-      total.push({
-        type: 'text',
-        text: row.value
-      })
-    }
-    return total
-  }, [])
-  const line = await tool.line.getClient(process.env.LINE_CHANNEL_ACCESS_TOKEN)
+  console.debug('[-] botEvent.contect.get')
+  // let rows = await tool
+  //   .db('setting')
+  //   .where('option', 'CONTACT_IMAGE')
+  //   .orWhere('option', 'CONTACT_MESSAGE')
+  //   .orderBy('option')
+  // if (rows.length != 2) return false
+  // let messages = rows.reduce((total, row) => {
+  //   if (row.option === 'CONTACT_IMAGE' && row.value) {
+  //     total.push({
+  //       type: 'image',
+  //       originalContentUrl: row.value,
+  //       previewImageUrl: row.value
+  //     })
+  //   }
+  //   if (row.option === 'CONTACT_MESSAGE' && row.value) {
+  //     total.push({
+  //       type: 'text',
+  //       text: row.value
+  //     })
+  //   }
+  //   return total
+  // }, [])
+  const contact = logic.setting.getContact()
+  let messages = []
+  if (contact.image) {
+    messages.push({
+      type: 'image',
+      originalContentUrl: contact.image,
+      previewImageUrl: contact.image
+    })
+  }
+  if (contact.text) {
+    messages.push({
+      type: 'text',
+      text: contact.text
+    })
+  }
+  const line = logic.line.getClient()
   await line.replyMessage(replyToken, messages)
   return true
 }
