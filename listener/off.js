fc48a11d24faab8afd4d173053d479e94ea471c0
Gucode <guc0d3@gmail.com>
2020-05-10 16:28:28 +0700

    fix error

--- listener/off.js
+++ listener/off.js
@@ -6,7 +6,7 @@ const TextType = require('../Type/Text')
 const listener = async (event) => {
   if (!event.message) return false
   if (event.message.type !== 'text') return false
-  if (event.message.text !== CommandType.on) return false
+  if (event.message.text !== CommandType.off) return false
   console.debug('call listener.on')
   await database('setting').where('option', 'BOT_STATUS').update({ value: '0' })
   await line.replyMessage(event.replyToken, TextType.botIsOff)
