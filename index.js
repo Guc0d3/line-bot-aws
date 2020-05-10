f70ea6eba46745d3efc1873550af724e5218dfdd
Gucode <guc0d3@gmail.com>
2020-05-10 14:36:13 +0700

    split listener

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const line = require('./line')
 const listener = require('./listener')
 
 if (process.env.APP_ENV === 'production') {
+  // production no log
   console.log = () => {}
   console.debug = () => {}
 } else if (process.env.APP_ENV === 'debug') {
@@ -11,52 +12,63 @@ if (process.env.APP_ENV === 'production') {
   console.debug = () => {}
 }
 
+// basic event
+// {
+//   "events": [{
+//     "type": "message",
+//     "replyToken": "50781340b00541cc...",
+//     "source": {
+//       "userId": "U3c28a70ed7c5e7ce2...",
+//       "groupId": "U3c28a70ed7c5e7ce2...",
+//       "type": "user" // or "group" or "room"
+//     },
+//     "timestamp": 1546848285013,
+//     "message": {
+//       "type": "text",
+//       "id": "9141452766858",
+//       "text": "Hello, world!"
+//     }
+//   }],
+//   "destination": "U820116ffcbe3f3ca7..."
+// }
 exports.handler = async (event) => {
-  // basic event
-  // {
-  //   "events": [{
-  //     "type": "message",
-  //     "replyToken": "50781340b00541cc...",
-  //     "source": {
-  //       "userId": "U3c28a70ed7c5e7ce2...",
-  //       "groupId": "U3c28a70ed7c5e7ce2...",
-  //       "type": "user" // or "group" or "room"
-  //     },
-  //     "timestamp": 1546848285013,
-  //     "message": {
-  //       "type": "text",
-  //       "id": "9141452766858",
-  //       "text": "Hello, world!"
-  //     }
-  //   }],
-  //   "destination": "U820116ffcbe3f3ca7..."
-  // }
+  // log event info
   const botEvent = event.events[0]
   console.debug('botEvent =', JSON.stringify(botEvent, null, 2))
-
   const user = await line.getProfileById(botEvent.source.userId)
   console.debug('user =', JSON.stringify(user, null, 2))
-
-  const works = [
-    // public listener
-    await listener.price(botEvent),
-    await listener.holiday(botEvent),
-    await listener.location(botEvent),
-    await listener.guide(botEvent),
-    await listener.register.getPrompt(botEvent),
-    await listener.register.activate(botEvent),
-    await listener.contact(botEvent),
+  // listener
+  let noEvent = false
+  if (event.source.groupId !== process.env.LINE_MASTER_OF_BOT_GROUP_ID) {
     // private listener
-    await listener.command(botEvent),
-    await listener.help(botEvent),
-    await listener.register.getCode(botEvent),
-    await listener.register.randomCode(botEvent),
-    await listener.reply(botEvent),
-    await listener.web(botEvent),
-  ]
-  const noEvent = !works.reduce((result, work) => {
-    return result || work
-  }, false)
+    const privateWorks = [
+      await listener.price(botEvent),
+      await listener.holiday(botEvent),
+      await listener.location(botEvent),
+      await listener.guide(botEvent),
+      await listener.register.getPrompt(botEvent),
+      await listener.register.activate(botEvent),
+      await listener.contact(botEvent),
+    ]
+    noEvent = !privateWorks.reduce((result, work) => {
+      return result || work
+    }, false)
+  } else {
+    // public listener
+    const publicWorks = [
+      await listener.price(botEvent),
+      await listener.holiday(botEvent),
+      await listener.location(botEvent),
+      await listener.guide(botEvent),
+      await listener.register.getPrompt(botEvent),
+      await listener.register.activate(botEvent),
+      await listener.contact(botEvent),
+    ]
+    noEvent = !publicWorks.reduce((result, work) => {
+      return result || work
+    }, false)
+  }
+  // echo other event
   if (noEvent) {
     await listener.echo(botEvent)
   }
